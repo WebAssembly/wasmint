ec0ad455ea0fdcbd9741042fb7174929fc2580dd
Raphael Isemann <teemperor@gmail.com>
2015-12-08 16:49:55 +0100

    Fixed testsuite script

--- runTestsuite.sh
+++ runTestsuite.sh
@@ -4,7 +4,7 @@ echo "#####################################"
 echo "###    Transforming wast files    ###"
 echo "#####################################"
 
-./testsuite_converter /home/teemperor/workspace/wasm/merge_wasm_module/wasmint/testsuite/*.wast
+./testsuite_converter ${CMAKE_SOURCE_DIR}/testsuite/*.wast
 
 totalTests=0
 failedTests=0
